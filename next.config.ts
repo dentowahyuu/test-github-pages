816d1ef7c7439cafe920d1d8ac585d1e72d55b44
dentowahyuu <wahyudento08@gmail.com>
2025-12-17 21:34:45 +0700

    fix: update image sources to absolute paths and simplify next config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/test-github-pages',
-  // assetPrefix memastikan CSS dan Gambar tidak hilang
-  assetPrefix: isProd ? '/test-github-pages/' : '', 
   images: {
     unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true, 
-  },
+  }
 };
 
 export default nextConfig;
