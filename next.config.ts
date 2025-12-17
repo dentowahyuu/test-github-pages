3a67a56fea2681ae01babd2eb67ff86669ef6878
dentowahyuu <wahyudento08@gmail.com>
2025-12-17 12:04:00 +0700

    fix 2

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Tambahkan kedua blok ini untuk memaksa build tetap jalan
   typescript: {
     ignoreBuildErrors: true, 
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
