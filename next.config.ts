76b76eaa18addf3823b329ec2d4444f7885856d2
dentowahyuu <wahyudento08@gmail.com>
2025-12-17 12:40:44 +0700

    update update

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // Ini akan memastikan link CSS/JS dan Gambar menggunakan subfolder repo
   basePath: '/test-github-pages',
+  // Tambahkan ini agar folder /out/ di GitHub lebih terstruktur
+  trailingSlash: true, 
   images: {
-    loader: 'custom',
-    loaderFile: './loader.ts', // Arahkan ke file yang baru dibuat
+    // Kita matikan loader custom agar kembali ke standar tapi dengan basePath
     unoptimized: true,
   },
   typescript: {
